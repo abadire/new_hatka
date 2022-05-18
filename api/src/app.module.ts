443068597a31d82da4a182be1426085659a280b0
evgen@DESKTOP-VHV99QJ <evgen@DESKTOP-VHV99QJ.localdomain>
2022-05-18 21:21:06 +0300

    dobavlenie bazidannih

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+const port = process.env.PORT;
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+      host: 'localhost',
+      port: port,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
